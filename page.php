75798765d71fd947d2d8fd3bfb43cf27c431bfc4
Grey McGowan <greymcgowan@gmail.com>
2018-07-04 22:08:42 +1000

    Installed Bootstrap

--- page.php
+++ page.php
@@ -17,7 +17,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+		
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -31,7 +31,7 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-
+		
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
