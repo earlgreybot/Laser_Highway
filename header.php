6207b798119d2dce26421402c4efe7d7d408465a
Grey McGowan <greymcgowan@gmail.com>
2018-08-17 16:56:05 +1000

    made "lowlight black"  (232122) a shade darker (1f1d1e) #20 excess whitespace to the right fixed using overflow-x: hidden; #24 added custom theme colour to head for android mobile

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#262425">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
